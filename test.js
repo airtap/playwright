e8c109065ce420d818bf0f654939c8148614ed62
Vincent Weevers <mail@vincentweevers.nl>
2020-08-23 18:24:59 +0200

    Skip webkit in travis

--- test.js
+++ test.js
@@ -4,4 +4,12 @@ const test = require('tape')
 const integration = require('airtap/test/integration')
 const Provider = require('.')
 
-integration(test, Provider)
+if (process.env.CI) {
+  integration(test, Provider, {
+    // Exclude webkit for now.
+    // TODO: fix webkit system dependencies on Travis
+    wanted: [{ name: 'chromium' }, { name: 'firefox' }]
+  })
+} else {
+  integration(test, Provider)
+}

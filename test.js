97c08f402c8d272df5046eb08d1b11fd3d2d38ec
Vincent Weevers <mail@vincentweevers.nl>
2020-08-23 18:36:09 +0200

    Restore webkit in travis

--- test.js
+++ test.js
@@ -4,12 +4,4 @@ const test = require('tape')
 const integration = require('airtap/test/integration')
 const Provider = require('.')
 
-if (process.env.CI) {
-  integration(test, Provider, {
-    // Exclude webkit for now.
-    // TODO: fix webkit system dependencies on Travis
-    wanted: [{ name: 'chromium' }, { name: 'firefox' }]
-  })
-} else {
-  integration(test, Provider)
-}
+integration(test, Provider)
